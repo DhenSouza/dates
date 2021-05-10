e4a76f99ca9d168a672e33e3dc08be520bc8306b
DhenSouza <ds052798ts@gmail.com>
2021-05-10 14:25:39 -0300

    feat(dateFormat): Implementando exercicio sobre o finalFormat

--- src/dateFormat/ExercicioFinalFormat.java
+++ src/dateFormat/ExercicioFinalFormat.java
@@ -3,14 +3,18 @@ package dateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ExemploDateFormat002 {
+public class ExercicioFinalFormat {
 
     public static void main(String[] args) {
+
         Date agora = new Date();
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:mmm");
+
 
         String dataFormatada = formatter.format(agora);
+
 
         System.out.println(dataFormatada);
         // 10/05/2021
